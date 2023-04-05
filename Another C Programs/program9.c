83b7b905b6207ee5f75a3996c5a109a2879a5d7f
devrohan0 <omegaaaa07@gmail.com>
2023-04-05 22:38:01 +0530

    Subtraction of 2 Matrices

--- Another C Programs/program9.c
+++ Another C Programs/program9.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/* #include <stdio.h>
 
 int main() {
     int m, n, i, j;
@@ -42,22 +42,65 @@ int main() {
 
     return 0;
 }
+*/
+
+
+/*This program first reads the size of the matrices and the elements 
+of the two matrices from the user. It then subtracts the second 
+matrix from the first matrix and stores the result in a third 
+matrix called diff. The subtraction of two matrices requires that 
+they have the same dimensions, so the program first checks if the 
+number of rows and columns of the two matrices are the same.
+
+The subtraction of two matrices is performed using a nested for loop. 
+The outer loop iterates over the rows of the matrices and the inner 
+loop iterates over the columns of the matrices. The difference of 
+the corresponding elements of the two matrices is then stored in 
+the corresponding element of the diff matrix.
+
+Finally, the program displays the difference of the two matrices 
+by iterating over the rows and columns of the diff matrix and 
+printing each element.*/
 
 
 
-// This program first reads the size of the matrices and the elements 
-// of the two matrices from the user. It then subtracts the second 
-// matrix from the first matrix and stores the result in a third 
-// matrix called diff. The subtraction of two matrices requires that 
-// they have the same dimensions, so the program first checks if the 
-// number of rows and columns of the two matrices are the same.
+#include <stdio.h>
 
-// The subtraction of two matrices is performed using a nested for loop. 
-// The outer loop iterates over the rows of the matrices and the inner 
-// loop iterates over the columns of the matrices. The difference of 
-// the corresponding elements of the two matrices is then stored in 
-// the corresponding element of the diff matrix.
+int main() {
+    int a[2][2], b[2][2], subt[2][2];
+    int i , j ;
 
-// Finally, the program displays the difference of the two matrices 
-// by iterating over the rows and columns of the diff matrix and 
-// printing each element.
+    // Read the elements of the first matrix
+    printf("Enter the elements of the first matrix:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Read the elements of the second matrix
+    printf("Enter the elements of the second matrix:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    printf("Difference of matrices:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            subt[i][j] = a[i][j] - b[i][j];
+            printf("%d ", subt[i][j]);
+        }
+        printf("\n");  // move to a new line after each row is printed
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
