65989368ecc2d7e11c62f797f8514ed6f4f984aa
Rohan Sharma <127294534+devrohan0@users.noreply.github.com>
2023-04-05 19:07:29 +0530

    Update program2.c
    updated by rohan

--- Another C Programs/program2.c
+++ Another C Programs/program2.c
@@ -1,1 +1,13 @@
 // write a program to take todays date and print tomorrows date
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t t = time(NULL);
+    struct tm *today = localtime(&t);
+    today->tm_mday += 1; // add one day
+    mktime(today); // normalize time
+
+    printf("Today's date: %d-%02d-%02d\n", today->tm_year + 1900, today->tm_mon + 1, today->tm_mday);
+    return 0;
+}
