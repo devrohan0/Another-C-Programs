fe05da79bc07f5edb89fff9f04267268e4603687
devrohan0 <omegaaaa07@gmail.com>
2023-04-05 19:20:49 +0530

    uploaded by Rohan

--- Another C Programs/program3.c
+++ Another C Programs/program3.c
@@ -1,1 +1,14 @@
 // write a program to take todays date and print yesterdays date
+
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t t = time(NULL);
+    struct tm *today = localtime(&t);
+    today->tm_mday -= 1; // subtract one day
+    mktime(today); // normalize time
+
+    printf("Yesterday's date: %d-%02d-%02d\n", today->tm_year + 1900, today->tm_mon + 1, today->tm_mday);
+    return 0;
+}
